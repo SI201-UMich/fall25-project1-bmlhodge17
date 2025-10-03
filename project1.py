434fcc5932999c3e869eee47441c4c774be6ede5
Brianna Hodge <bmlhodge@umich.edu>
2025-10-03 11:47:39 -0400

    added csv for data set and starter code

---
+++ project1.py
@@ -0,0 +1,31 @@
+'''
+student info/collaborators/etc
+import libraries 
+create a function to read in penguins csv file, use 'r' 
+define two analysis functions that create and populate(add key, values to) a dictionary, and calcuates 
+create a function that outputs the results in a new text or csv file (use text for a dictionary, or use csv for calculation/if output looks more like a "list")
+create a class for unittest cases
+define main function that calls in the testcases
+'''
+
+import os
+import unittest
+
+
+
+'''
+to write test cases function
+write four testcases per function (8 total)
+we don't have a function written yet so its a "black box"
+for testcase function reference discussion 5 SLIDES
+
+
+https://www.geeksforgeeks.org/python/unit-testing-python-unittest/
+#test example
+class TestUnitest(unittest):
+    def test_functionname(self):
+        a = functionname(6,3)
+        b = 2
+        self.assertEqual(a, b) #checks if a and b are equal
+        
+'''
